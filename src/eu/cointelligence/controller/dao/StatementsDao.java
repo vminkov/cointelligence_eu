80ae1a151530638e5e63d7f80a44d0d826bd96b9
Veliko Minkov <vichobg@gmail.com>
2013-04-28 12:51:34 +0300

    Some DAOs (a generic one included)

---
+++ src/eu/cointelligence/controller/dao/StatementsDao.java
@@ -0,0 +1,19 @@
+package eu.cointelligence.controller.dao;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import eu.cointelligence.model.Statement;
+
+@Stateless
+public class StatementsDao {
+	
+	@PersistenceContext(unitName = "COINTELLIGENCE")
+	private EntityManager em;
+	
+	public String getStatementById(String id){
+		System.out.println(em != null);
+		return new String(new Boolean(em != null).toString());
+	}
+} 
