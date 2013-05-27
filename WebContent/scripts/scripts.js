6f45a29e48e6a06afeb8e05885462c4a14d2ffaf
boncho valkov <bonchovylkov@abv.bg>
2013-05-27 03:06:34 +0300

    add about us and modal window for questions
    yep

--- WebContent/scripts/scripts.js
+++ WebContent/scripts/scripts.js
@@ -1,4 +1,4 @@
-(function index() {
+﻿(function index() {
     var paths = {};
     paths.stetements = 'testJSON/testStatements.js';
     paths.users = 'testJSON/users.js';
@@ -7,9 +7,63 @@
             var row = $('<tr class="questionRow"/>');
             row.append("<td class='idCol questionCol'>" + obj.id + "</td>");
             row.append("<td class='titleCol questionCol'> " + obj.title + "</td>");
-            row.append("<td class='descCol questionCol'><i class='icon-info-sign'></i> <a href=#>" + obj.description + "</a></td>");
+            row.append("<td class='descCol questionCol'><i class='icon-info-sign'></i> <a href='#statement" + obj.id + "' role='button' data-toggle='modal'>" + obj.description + "</a></td>");
             row.append("<td class='priceCol questionCol'>" + obj.currentValue + "</td>");
             $('#statementsInfo').append(row);
+            var modalQuestion = document.createElement("div");
+            modalQuestion.id = "statement" + obj.id;
+            modalQuestion.className = "modal hide fade";
+            modalQuestion.tabIndex = "-1";
+            modalQuestion.setAttribute("role","dialog");
+            modalQuestion.setAttribute("aria-labelledby", "myModalLabel");
+            modalQuestion.setAttribute("aria-hidden", "true");
+            modalQuestion.innerHTML ="<div class='modal-header'>"+
+                "<button type='button' class='close' data-dismiss='modal' aria-hidden='true'>×</button>" +
+                "<h3 id='H2'>Търгувай с въпроса!</h3>" +
+                "</div>" +
+                "<div class='modal-body'>" +
+                "<form class='form-horizontal'>" +
+                "<div class='control-group'>" +
+                "<label class='control-label'>Заглавие на въпроса</label>" +
+                "<div class='controls'>" +
+                "<p>"+obj.title + "<p/>" +
+                "</div>" +
+                "</div>" +
+                "<div class='control-group'>" +
+                "<label class='control-label'>Описание на въпроса</label>" +
+                "<div class='controls'>" +
+                "<p>" + obj.description + "<p/>" +
+                "</div>" +
+                "</div>" +
+                 "<div class='control-group'>" +
+                "<label class='control-label'>Текуща цена на въпроса:</label>" +
+                "<div class='controls'>" +
+                "<p class='btn btn-info'>" + obj.currentValue + " cointels за акция" + "<p/>" +
+                "</div>" +
+                "</div>" +
+                "<div class='control-group'>" +
+                "<label class='control-label'>Избери твърдерние с определена цена:</label>" +
+                "<div class='controls stetementContainer'>" +
+                "<i class='icon-hand-up'></i>" +
+                "<input type='radio' id='yesStetement' name='stetement' value='yes'> Да, ще се случи и искам да си купя:" +
+                "<input type='text' id='yesQuontity'  style='width:50px' /><br/><br/>" +
+                "<i class='icon-hand-down'></i>" +
+                 "<input type='radio'id='noStetement' name='stetement' value='no'> Не, няма да се случи и искам да си купя:" +
+                 "<input type='text' id='noQuontity'  style='width:50px'/><br/>" +
+                "</div>" +
+                "</div>" +
+                   "<div class='control-group'>" +
+                "<label class='control-label'>Общ брой налични Cointels в момента:</label>" +
+                "<div class='controls'>" +
+                "<p class='btn btn-info'>" + 1000 + " cointels" + "<p/>" +
+                "</div>" +
+                "</div>" +
+                "</form>" +
+                "</div>" +
+                "<div class='modal-footer'>" +
+                "<button class='btn' data-dismiss='modal' aria-hidden='true'>Затвори</button>" +
+                "<button class='btn btn-primary'>Направи сделката!</button></div>";
+            $('#wrapper').append(modalQuestion);
         });
     }).fail(function () {
         console.log("fail");
@@ -19,7 +73,7 @@
         $.each(data, function (index, obj) {
             var row = $('<tr class="questionRow"/>');
             row.append("<td class='nicknameCol questionCol'><i class='icon-user'></i>" + obj.userName + "</td>");
-            row.append("<td class='moneyCol questionCol'> " + obj.account.money + "</td>");
+            //row.append("<td class='moneyCol questionCol'> " + obj.account.money + "</td>");
             row.append("<td class='priceCol questionCol'>" + obj.account.cointels + "</td>");
             $('#rankTable').append(row);
         });
@@ -28,12 +82,14 @@
     });
 }());
 
+
+
 (function activeNav() {
     var nav = document.getElementById("mainUl");
     var navElements = nav.children;
     function onNavItemClick(event) {
         event.stopPropagation();
-        event.preventDefault();
+        //event.preventDefault();
         for (var i = 0; i < navElements.length; i++) {
             navElements[i].className = "";
         }
@@ -47,6 +103,24 @@
 }());
 
 
+
+
+//function setStetementQuontity() {
+//    var stementsContainer = document.getElementsByClassName;
+//    for (var i = 0; i < stementsContainer.length; i++) {
+//        var stetementYesRadio = document.getElementById("yesStetement");
+//        if (stetementYesRadio.selected="selected") {
+//            var quontityYes = document.getElementById("yesQuontity");
+//            quontityYes.style.display = "block";
+//        }
+//        var stetementNoRadio = document.getElementById("noStetement");
+//        if (stetementNoRadio.selected = "selected") {
+//            var quontityYes = document.getElementById("noQuontity");
+//            quontityYes.style.display = "block";
+//        }
+//    }
+
+//};
 
 //var paths = {};
 //paths.stetements = '../testJSON/testStatements.js';
